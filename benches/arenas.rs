2269f79d02dbc10d0fabd2c6984ee71329962f39
Emile van Reenen <9017146+emilevr@users.noreply.github.com>
2023-10-23 22:14:00 +0200

    Reduce the sample size to limit memory usage in the arena benchmark

--- benches/arenas.rs
+++ benches/arenas.rs
@@ -33,6 +33,7 @@ pub struct IdArenaItem {
 pub fn bench_arenas(c: &mut Criterion) {
     let mut group = c.benchmark_group("arenas");
     group.measurement_time(Duration::from_secs(3));
+    group.sample_size(50);
     group.bench_function("std", |b| {
         let mut items = vec![];
         b.iter(|| {
