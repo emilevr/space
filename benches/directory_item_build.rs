5964a1c67940c173ff560fb94799a20d7ac6a3f7
Emile van Reenen <9017146+emilevr@users.noreply.github.com>
2023-10-27 08:45:04 +0200

    Add and update space benchmark

---
+++ benches/directory_item_build.rs
@@ -0,0 +1,24 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use space_rs::DirectoryItem;
+use std::path::Path;
+
+const DIRECTORY_PATH: &str = "./tmp.sample";
+
+pub fn directory_item_build(c: &mut Criterion) {
+    let path = &Path::new(DIRECTORY_PATH).to_path_buf();
+    c.bench_function(
+        &format!("DirectoryItem::build() on {}", path.display()),
+        |b| {
+            b.iter(|| {
+                black_box(DirectoryItem::build(vec![path.clone()]));
+            })
+        },
+    );
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = directory_item_build
+}
+criterion_main!(benches);
