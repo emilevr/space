c36bfc45206598b2cf3f3e1b6d9b336481c5135c
Emile van Reenen <9017146+emilevr@users.noreply.github.com>
2023-11-03 09:16:14 +0200

    Workflow refactoring to facilitate install options and addition of some install scripts

---
+++ .github/actions/package-windows/update-scoop-manifest.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)][string]$Target,
+    [Parameter(Mandatory=$true)][string]$ArchiveFileHash,
+    [Parameter(Mandatory=$true)][string]$Version
+)
+
+$artifactsDir = 'artifacts/dist/pages/scoop'
+$sourceTemplatePath = 'install/scoop/space.json'
+
+New-Item -Type Directory -Path $artifactsDir -Force | Out-Null
+
+$content = Get-Content $sourceTemplatePath
+
+$result = $content | Select-String -Pattern '0\.0\.0'
+foreach ($match in $result.Matches) {
+    $content = $content -replace [regex]::Escape($match.Value), $Version
+}
+
+$result = $content | Select-String -Pattern '000'
+foreach ($match in $result.Matches) {
+    $content = $content -replace [regex]::Escape($match.Value), $ArchiveFileHash
+}
+
+$content | Out-File -FilePath "$artifactsDir/space.json" -Encoding utf8 -Force
