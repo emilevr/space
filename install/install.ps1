c84a9c64732b9a8800edfa0ee20df823f4c169ca
Emile van Reenen <9017146+emilevr@users.noreply.github.com>
2023-11-04 13:36:23 +0200

    Improve install scripts

--- install/install.ps1
+++ install/install.ps1
@@ -8,35 +8,34 @@ function Install-LinuxOrMacOS {
     )
 
     $installDirPath = "/usr/local/bin"
-    Write-Host "Installing space CLI to $installDirPath." -NoNewline
+    Write-Host "👷 Installing space CLI to $installDirPath." -NoNewline
     Write-Host " This will typically require root access." -ForegroundColor Cyan
 
     $latestReleaseArchive = "https://github.com/emilevr/space/releases/latest/download/$ArchiveFilename"
-    $archiveFilePath = "~/$ArchiveFilename"
+    $archiveFilePath = "$HOME/$ArchiveFilename"
 
-    $pathSeparator = ':'
-
-    $extractDirPath = '~'
+    $extractDirPath = '$HOME'
     $extractBinaryPath = "$extractDirPath/space"
     $installedBinaryPath = "$installDirPath/space"
 
     try {
-        Write-Host "Downloading the latest version from $latestReleaseArchive"
+        Write-Host "⭳⭳ Downloading the latest version from $latestReleaseArchive"
         Invoke-WebRequest -Uri $latestReleaseArchive -OutFile $archiveFilePath | Out-Null
 
-        Write-Host "Extracting archive $archiveFilePath to $extractDirPath"
+        Write-Host "👷 Extracting archive $archiveFilePath to $extractDirPath"
         tar -xzf $archiveFilePath -C $extractDirPath/
 
-        Write-Host "Making space executable"
+        Write-Host "👷 Making space executable"
         chmod +x $extractBinaryPath
 
-        Write-Host "Moving $extractBinaryPath to $installedBinaryPath." -NoNewline
+        Write-Host "👷 Moving $extractBinaryPath to $installedBinaryPath." -NoNewline
         Write-Host " This will typically fail without root access." -ForegroundColor Cyan
         Move-Item -Path $extractBinaryPath -Destination $installedBinaryPath -Force
 
         return $true
     } finally {
-        Write-Host "Removing downloaded archive file $archiveFilePath"
+        Write-Host "👷 Cleaning up:"
+        Write-Host "   Removing downloaded archive file $archiveFilePath"
         Remove-Item -Path $archiveFilePath -Force -ErrorAction SilentlyContinue
     }
 
@@ -48,39 +47,41 @@ function Install-Windows {
     param()
 
     $installDirPath = Join-Path -Path $HOME -ChildPath 'space'
+
     Write-Host "Installing space CLI to $installDirPath"
 
     New-Item -Type Directory -Path $installDirPath -Force | Out-Null
 
     $archiveFilename = 'space-x86_64-pc-windows-msvc.zip'
-    $archiveFilePath = "~/$archiveFilename"
+    $archiveFilePath = "$HOME/$archiveFilename"
     $latestReleaseArchive = "https://github.com/emilevr/space/releases/latest/download/$archiveFilename"
     $pathSeparator = ';'
 
     try {
-        Write-Host "Downloading the latest version from $latestReleaseArchive"
+        Write-Host "⭳⭳ Downloading the latest version from $latestReleaseArchive"
         Invoke-WebRequest -Uri $latestReleaseArchive -OutFile $archiveFilePath | Out-Null
 
-        Write-Host "Extracting archive $archiveFilePath to $installDirPath"
+        Write-Host "👷 Extracting archive $archiveFilePath to $installDirPath"
         Expand-Archive -Path $archiveFilePath -DestinationPath $installDirPath -Force
 
-        Write-Host "Adding the space CLI to the path"
+        Write-Host "👷 Adding the space CLI to the path"
         $escapedPath = [regex]::Escape($installDirPath)
         $result = $env:PATH | Select-String -Pattern "$escapedPath\\?"
         if ($result.Matches.Success) {
-            Write-Host "  The PATH variable already contains the required entry. Not modifying PATH."
+            Write-Host "   The PATH variable already contains the required entry. Not modifying PATH."
         } else {
-            Write-Host "  Updating PowerShell session path environment variable"
+            Write-Host "   Updating PowerShell session path environment variable"
             $pathEntries = $env:PATH -split $pathSeparator
             $env:PATH = ($pathEntries + $installDirPath) -join $pathSeparator
 
-            Write-Host "  Updating user path environment variable"
+            Write-Host "   Updating user path environment variable"
             [System.Environment]::SetEnvironmentVariable('PATH', $env:PATH, [System.EnvironmentVariableTarget]::User)
         }
 
         return $true
     } finally {
-        Write-Host "Removing downloaded archive file $archiveFilePath"
+        Write-Host "👷 Cleaning up:"
+        Write-Host "   Removing downloaded archive file $archiveFilePath"
         Remove-Item -Path $archiveFilePath -Force -ErrorAction SilentlyContinue
     }
 
@@ -96,9 +97,9 @@ if ($IsLinux) {
 } elseif ($IsWindows) {
     $installed = Install-Windows
 } else {
-    Write-Error "Only Linux, MacOS and Windows installs are supported."
+    Write-Error "❌ Only Linux, MacOS and Windows installs are supported."
 }
 
 if ($installed) {
-    Write-Host "Installation completed. Run 'space --help' to see a list of available options." -ForegroundColor Green
+    Write-Host "✔️ Installation completed. Run 'space --help' to see a list of available options." -ForegroundColor Green
 }
--- install/install.sh
+++ install/install.sh
@@ -13,19 +13,26 @@ BINARY_FILE_PATH=$ARCHIVE_DIR/space
 INSTALL_DIR=/usr/local/bin
 INSTALL_FILE_PATH=$INSTALL_DIR/space
 
-echo "Downloading the latest binary from $ARCHIVE_FILE_URL ..."
-curl -L -o $ARCHIVE_FILE_PATH $ARCHIVE_FILE_URL
+echo "Installing space CLI to $INSTALL_DIR"
 
-echo "Extacting downloaded file $ARCHIVE_FILE_PATH"
-tar -xzvf $ARCHIVE_FILE_PATH -C $ARCHIVE_DIR/
+echo "⭳⭳ Downloading the latest binary from $ARCHIVE_FILE_URL ..."
+(curl -L -o $ARCHIVE_FILE_PATH $ARCHIVE_FILE_URL) || \
+    { echo "❌ Download failed!"; exit 1; }
 
-echo "Making the binary executable"
-chmod +x $BINARY_FILE_PATH
+echo "👷 Extacting downloaded file $ARCHIVE_FILE_PATH"
+(tar -xzvf $ARCHIVE_FILE_PATH -C $ARCHIVE_DIR/) || \
+    { echo "❌ Could not extract archive!"; exit 2; }
 
-echo "Moving $BINARY_FILE_PATH to $INSTALL_DIR"
-sudo mv $BINARY_FILE_PATH $INSTALL_FILE_PATH
+echo "👷 Making the binary executable"
+(chmod +x $BINARY_FILE_PATH) || \
+    { echo "❌ Could not make space executable!"; exit 3; }
 
-echo "Cleaning up..."
-rm -f $ARCHIVE_FILE_PATH
+echo "👷 Moving $BINARY_FILE_PATH to $INSTALL_DIR"
+(sudo mv $BINARY_FILE_PATH $INSTALL_FILE_PATH) || \
+    { echo "❌ Failed to move $BINARY_FILE_PATH to $INSTALL_DIR!"; exit 4; }
 
-echo "Installation completed. Run 'space --help' to see a list of available options."
+echo "👷 Cleaning up..."
+(rm -f $ARCHIVE_FILE_PATH) || \
+    { echo "❌ Cleanup failed!"; exit 5; }
+
+echo "✔️ Installation completed. Run 'space --help' to see a list of available options."
